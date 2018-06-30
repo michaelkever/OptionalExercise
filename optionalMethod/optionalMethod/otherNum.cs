2257363764be8e11a96498dde702ec5a906d0f43
Michael Kever <michaelkever@gmail.com>
2018-06-30 12:32:04 -0500

    Start of exercise
    on page 63

---
+++ optionalMethod/optionalMethod/otherNum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace optionalMethod
+{
+    class otherNum
+    {
+        public static string AddNumbers(int a, string b = "100")
+        {
+            //return $" a = {a}, b = {b}, a + b = {a + b}";
+            
+            int tempValue01 = a ;
+            int tempValue02 = Convert.ToInt32(b);;
+            int tempValue = tempValue01 + tempValue02;
+            string tempTotal = Convert.ToString(tempValue);
+            
+            return tempTotal;
+            
+        }
+
+       
+    }
+}
